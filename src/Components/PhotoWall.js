064699d3519866fb68daf2b4a7a3cfdfbd8ec7f3
Kshitij Kumar <kshitijsingla7890@gmail.com>
2020-05-31 00:49:16 +0530

    first commit and classes converted to stateless functions

---
+++ src/Components/PhotoWall.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Photo from './Photo';
+
+
+
+function PhotoWall(props){
+  return (<div className="photoGrid">
+          {props.posts.map((post,index) => <Photo key={index} post={post}/>)}
+        </div>);
+}
+
+
+
+export default PhotoWall
